15c39325292b699d4129ef1ed65bcac34fb29694
jovianalthaf <90819182+jovianalthaf@users.noreply.github.com>
2021-09-25 21:17:55 +0700

    Add files via upload

---
+++ Tugas luas segitiga.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+main()
+{
+	/*Deklarasi*/
+	
+	float alas;
+	float tinggi;
+	float luas;
+	
+	/*Algoritma*/
+	cout<< "Input Alasnya berapa = ";
+	cin>>alas;
+	cout<< "Input Tinggi berapa = ";
+	cin>>tinggi;
+	luas = 0.5 *alas * tinggi;
+	cout>>"Luas Segitiga adalah " << luas << endl;
+	
+	return 0;
+	
+
+	
+	
+
+}
